35141491bb79b1132db34c02e7c826b59680f70c
Fariahh Basha <92734995+fari03@users.noreply.github.com>
2023-05-13 11:36:46 +0530

    Create lowercase_converter.py

---
+++ Python/lowercase_converter.py
@@ -0,0 +1,10 @@
+while True:
+    # Read a line of input
+    line = input()
+
+    # If the line is blank, exit the loop
+    if line == "":
+        break
+
+    # Convert the line to lowercase and print it
+    print(line.lower())

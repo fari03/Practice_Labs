9b96a8865e5b6ef8d3f69a7721de61bb11900c50
Fariahh Basha <92734995+fari03@users.noreply.github.com>
2023-05-30 23:51:42 +0530

    Create d3.js

---
+++ AWT/ES6/d3.js
@@ -0,0 +1,12 @@
+const person = {
+    name: 'Alice',
+    age: 25,
+    country: 'USA',
+    occupation: 'Engineer'
+  };
+  
+  const { name, age } = person;
+  
+  console.log(name); // Output: Alice
+  console.log(age); // Output: 25
+  

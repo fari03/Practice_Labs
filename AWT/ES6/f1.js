d0f662420755bdb09b1c7debd33c6e507b5a0763
Fariahh Basha <92734995+fari03@users.noreply.github.com>
2023-05-30 23:52:01 +0530

    Create f1.js

---
+++ AWT/ES6/f1.js
@@ -0,0 +1,10 @@
+// Traditional function
+function square(x) {
+  return x * x;
+}
+
+// Fat arrow function
+const squareArrow = x => x * x;
+
+console.log(square(5)); // Output: 25
+console.log(squareArrow(5)); // Output: 25
